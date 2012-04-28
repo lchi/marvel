611cc0a10b86ea7990a44f64c03f4cf346158c83
Isaac Gonzalez <hyperneato@gmail.com>
2012-04-28 13:41:43 -0400

    returning requests via a prompt

--- http_server.scm
+++ http_server.scm
@@ -4,7 +4,21 @@
     
 (define (accept-loop socket)
   (let ((client (tcp-server-connection-accept socket #t #f)))
-    (display (read-char client))
+    (display (read-request client))
+    (write-response client)
     (close-port client)
     (accept-loop socket)))
 
+
+(define (read-request c)
+    (read-line c))
+
+(define (write-response c)
+   (begin 
+    (display "\n> ")
+    (display "HTTP/1.0 200 OK\n\n" c)
+    (display (read-line (interaction-i/o-port)) c)
+    
+    ))   
+    
+    
