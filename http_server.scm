a39e32b9fb2d735cc5729d9de36daf224c9da8ce
Lucas Chi <chi.lucas@gmail.com>
2012-04-28 17:06:14 -0400

    serving files!

--- http_server.scm
+++ http_server.scm
@@ -3,33 +3,43 @@
     (accept-loop socket)))
     
 (define (accept-loop socket)
-  (let ((client (tcp-server-connection-accept socket #t #f)))
-;;    (display (read-request client))
-    (display (acquire-GET-path (read-total-request client "")))
-    (write-response client)
+  (let* ((client (tcp-server-connection-accept socket #t #f))
+	(request (read-total-request client ""))
+	(request-path (acquire-GET-path request)))
+    (display request-path)
+    (newline)
+    (handle-request client request-path)
     (close-port client)
     (accept-loop socket)))
+
+(define (handle-request client request-path)
+  (cond ((string=? "" request-path) (handle-request client "index.html"))
+	((not (file-exists? request-path)) (display "HTTP/1.0 404 WAAT?\n\nGO AWAY! (404)" client))
+	(else (write-response client request-path))))
 
 ;; allow a default value for a response string
 (define (read-total-request client response)
     (let ((line (read-line client)))
       (if (string=? "" line)
 	  response
-	    (read-total-request client (string-append response line)))))
+	  (read-total-request client (string-append response line)))))
 
 (define (acquire-GET-path request-string)
   (let* ((slash-index (string-search-forward "/" request-string))
 	 (space-index (substring-search-forward " " request-string slash-index (string-length request-string))))
-    (substring request-string slash-index space-index)))
-    
+    (substring request-string (+ slash-index 1) space-index)))
 
-(define (write-response c)
+(define (write-response c request-path)
    (begin 
-    (display "\n> ")
-    (display "HTTP/1.0 200 OK\n\n" c)
-    (display (read-line (interaction-i/o-port)) c)
-    ))   
+     (display "HTTP/1.0 200 OK\n\n" c)
+     (send-file c request-path)))
     
+(define (send-file client filename)
+  (let ((file (open-input-file filename)))
+    (let loop ((ch (read-char file)))
+      (if (eof-object? ch)
+	  '()
+	  (begin
+	    (display ch client)
+	    (loop (read-char file)))))))
 
-
-    
