d344d5b0090d92a11e73e07fc1358d6c65411715
Isaac Gonzalez <hyperneato@gmail.com>
2012-04-28 16:16:40 -0400

    reading request path

--- http_server.scm
+++ http_server.scm
@@ -4,21 +4,32 @@
     
 (define (accept-loop socket)
   (let ((client (tcp-server-connection-accept socket #t #f)))
-    (display (read-request client))
+;;    (display (read-request client))
+    (display (acquire-GET-path (read-total-request client "")))
     (write-response client)
     (close-port client)
     (accept-loop socket)))
 
+;; allow a default value for a response string
+(define (read-total-request client response)
+    (let ((line (read-line client)))
+      (if (string=? "" line)
+	  response
+	    (read-total-request client (string-append response line)))))
 
-(define (read-request c)
-    (read-line c))
+(define (acquire-GET-path request-string)
+  (let* ((slash-index (string-search-forward "/" request-string))
+	 (space-index (substring-search-forward " " request-string slash-index (string-length request-string))))
+    (substring request-string slash-index space-index)))
+    
 
 (define (write-response c)
    (begin 
     (display "\n> ")
     (display "HTTP/1.0 200 OK\n\n" c)
     (display (read-line (interaction-i/o-port)) c)
-    
     ))   
     
+
+
     
